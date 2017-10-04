60eb19a5bfce35aaf1cb274a498d0ef122767cf1
Izabella-Timmons <bellaiza631@gmail.com>
2017-10-04 18:46:50 -0400

    Deleting comments

--- js/designs.js
+++ js/designs.js
@@ -1,11 +1,12 @@
 // Select color input
 // Select size input
 //When size is submitted by the user, call makeGrid()
-let size = document.getElementById("sizePicker");
-let height = document.getElementById("input_height");
-let width = document.getElementById("input_width");
-let color = document.getElementById("colorPicker");
-let grid = document.getElementById("pixel_canvas");
+
+const size = document.getElementById("sizePicker");
+const height = document.getElementById("input_height");
+const width = document.getElementById("input_width");
+const color = document.getElementById("colorPicker");
+const grid = document.getElementById("pixel_canvas");
 
 
 
@@ -28,14 +29,6 @@ function makeGrid(height, width) {
                 cell.style.backgroundColor = colorPicker.value;
             });
 
-            //row.appendChild(cell); //Insert content, specified by the parameter, to the end of each element
         }
     }
 }
-//const $colorPicker = document.getElementById("colorPicker");
-//let width = document.getElementById("input_width").value;
-//then you need to pass them to a function called
-//and you also need to define this function
-//now for that you will have to think of the logic
-//and also you need a fnction to change color when clicked
-//so also make a small function for that
